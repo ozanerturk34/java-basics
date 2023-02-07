7972949552cce57f2ecf92f7665a9907e89fe9cc
Ozan Erturk <ozan@relive.cc>
2023-02-07 19:48:42 +0100

    1.4: Strings cont.

--- src/StringClass.java
+++ src/StringClass.java
@@ -1,11 +1,16 @@
 public class StringClass {
     public static void printString() {
         int aNumber = 5;
+        double aDouble = 9.5;
         java.lang.String text = "Hi";
-        java.lang.String blank = "";
+        java.lang.String blank = " ";
         java.lang.String moreText = "Bob";
+        java.lang.String hiThere = text+blank+moreText; // can add strings to each other
         System.out.println(aNumber);
         System.out.println(text);
         System.out.println(text+blank+moreText);
+        System.out.println("Hi"+" "+"Bob");
+        System.out.println(hiThere);
+        System.out.println("My Number: "+aNumber+" "+aDouble); // can add numbers to string
     }
 }
