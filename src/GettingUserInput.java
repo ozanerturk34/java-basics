97f7ce95223b2436ad2d9364f60572d209cbdd02
Ozan Erturk <ozan@relive.cc>
2023-02-07 20:40:05 +0100

    1.9: Comments, Variable Scope, Do While Loops

--- src/GettingUserInput.java
+++ src/GettingUserInput.java
@@ -2,9 +2,22 @@ import java.util.Scanner;
 
 public class GettingUserInput {
     public static void print() {
-        Scanner input = new Scanner(System.in);
+
+        // Following code belongs to a previous lecture
+
+        /**
         System.out.println("Enter an decimel number: ");
         double aDouble = input.nextDouble();
         System.out.println("You have entered " + aDouble);
+         */
+
+        Scanner input = new Scanner(System.in);
+        int num;
+        do {
+            System.out.println("Enter a number: ");
+            num = input.nextInt();
+        } while(num < 9);
+        System.out.println("Your number is "+num+" which is bigger than 8");
+
     }
 }
