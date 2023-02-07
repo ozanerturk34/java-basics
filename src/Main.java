95d5353fb4dd3ebc83a8f54bde088467de89296b
Ozan Erturk <ozan@relive.cc>
2023-02-07 19:17:28 +0100

    1.3: Variables cont.

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,18 @@ public class Main {
         long aLong = 64;
         double aDouble = 1.123;
         float aFloat = 5.23f;
+
+        char aChar = 'a';
+        boolean aBool = true;
+        byte aByte = 127;
+
         System.out.println(aShort);
         System.out.println(aValue);
         System.out.println(aLong);
         System.out.println(aDouble);
         System.out.println(aFloat);
+        System.out.println(aChar);
+        System.out.println(aBool);
+        System.out.println(aByte);
     }
 }
