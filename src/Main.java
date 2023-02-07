7c3f1445104a1e9050fb29256e47ec40df50fe7f
Ozan Erturk <ozan@relive.cc>
2023-02-07 19:14:58 +0100

    Seciton 1-3: Variables

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int aValue = 32;
+        short aShort = 16;
+        long aLong = 64;
+        double aDouble = 1.123;
+        float aFloat = 5.23f;
+        System.out.println(aShort);
+        System.out.println(aValue);
+        System.out.println(aLong);
+        System.out.println(aDouble);
+        System.out.println(aFloat);
+    }
+}
